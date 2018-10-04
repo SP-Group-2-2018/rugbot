519fc5aeddc4d50bb7180fc89708104f91ca67e5
Stefanus <stefbuys21@gmail.com>
2018-10-04 19:05:55 +0200

    Register page

---
+++ rugbot/src/pages/register/register.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, LoadingController, Loading, AlertController  } from 'ionic-angular';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthProvider } from '../../providers/auth/auth';
+
+import { EmailValidator } from '../../validators/email';
+import { HomePage } from '../home/home';
+
+/**
+ * Generated class for the RegisterPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html',
+})
+export class RegisterPage {
+
+  registerForm: FormGroup;
+  loading: Loading;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public auth: AuthProvider, public FormBuilder: FormBuilder, public alertCtrl: AlertController, public loadingCtrl: LoadingController) {
+    this.registerForm = FormBuilder.group({
+      email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+      password: ['', Validators.compose([Validators.minLength(6), Validators.required])],
+      passwordConfirm: ['', Validators.required],
+    }, {validator: this.matchPassword('password', 'passwordConfirm')});
+  }
+
+  registerUser() {
+    if (!this.registerForm.value) {
+      console.log(this.registerForm.value);
+    } else {
+      this.auth.registerUser(this.registerForm.value.email, this.registerForm.value.password)
+        .then(auth => {
+          this.navCtrl.setRoot(HomePage);
+        }, error => {
+          this.loading.dismiss().then(() => {
+            let alert = this.alertCtrl.create({
+              message: error.message,
+              buttons: [
+                {
+                  text: "Ok",
+                  role: 'cancel'
+                }
+              ]
+            });
+            alert.present();
+          });
+        });
+
+      this.loading = this.loadingCtrl.create({
+        dismissOnPageChange: true,
+      });
+      this.loading.present();
+    }
+  }
+
+  // passwordSame: boolean = false;
+  matchPassword(password: string, passwordConfirm: string) {
+    return (group: FormGroup): {[key: string]: any} => {
+      let pass = group.controls[password];
+      let passCon = group.controls[passwordConfirm];
+
+      if (pass.value !== passCon.value) {
+        return {
+          mismatchedPasswords: true
+        };
+      }
+    }
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegisterPage');
+  }
+
+}
